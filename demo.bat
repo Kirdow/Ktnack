04dc184fad863b262663e6b37c85378e8dc830c7
Kirdow <10155603+Kirdow@users.noreply.github.com>
2023-03-15 20:22:05 +0100

    fix demo.bat

--- demo.bat
+++ demo.bat
@@ -4,10 +4,13 @@ set "id=%~1"
 
 if %id%==1 (
     cargo run -- demo/01-arithmetic.ktnck
+    ktnckc
 ) else if %id%==2 (
     cargo run -- demo/02-if-statement.ktnck
+    ktnckc
 ) else if %id%==3 (
     cargo run -- demo/03-while.ktnck
+    ktnckc
 ) else (
     echo Table Of Content:
     echo 1 - Arithmetic
